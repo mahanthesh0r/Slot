eb3ba6197e3a9b02fea0581d946c08267f79a621
Mahanthesh <monty.officio@gmail.com>
2018-08-04 23:04:28 +0530

    Initial Commit

---
+++ Slot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slot
+//
+//  Created by Mahanthesh R on 04/08/18.
+//  Copyright © 2018 Mahanthesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
