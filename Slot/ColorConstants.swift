e77933b5c79de51b7366f968ef9b6d046fa741ba
Mahanthesh <monty.officio@gmail.com>
2018-08-05 16:48:22 +0530

    Firebase Authentication Integration and Login page design

---
+++ Slot/ColorConstants.swift
@@ -0,0 +1,23 @@
+//
+//  ColorConstants.swift
+//  Slot
+//
+//  Created by Mahanthesh R on 05/08/18.
+//  Copyright © 2018 Mahanthesh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors{
+    static let brightOrange     = UIColor(red: 255.0/255.0, green: 69.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let red              = UIColor(red: 255.0/255.0, green: 115.0/255.0, blue: 115.0/255.0, alpha: 1.0)
+    static let orange           = UIColor(red: 255.0/255.0, green: 175.0/255.0, blue: 72.0/255.0, alpha: 1.0)
+    static let blue             = UIColor(red: 74.0/255.0, green: 144.0/255.0, blue: 228.0/255.0, alpha: 1.0)
+    static let green            = UIColor(red: 91.0/255.0, green: 197.0/255.0, blue: 159.0/255.0, alpha: 1.0)
+    static let darkGrey         = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+    static let veryDarkGrey     = UIColor(red: 13.0/255.0, green: 13.0/255.0, blue: 13.0/255.0, alpha: 1.0)
+    static let lightGrey        = UIColor(red: 200.0/255.0, green: 200.0/255.0, blue: 200.0/255.0, alpha: 1.0)
+    static let black            = UIColor(red: 0.0/255.0, green: 0.0/255.0, blue: 0.0/255.0, alpha: 1.0)
+    static let white            = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+}
--- Slot/ViewController.swift
+++ Slot/ViewController.swift
@@ -7,12 +7,36 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    
+    
+    @IBAction func LoginAction(_ sender: UIButton) {
+        if EmailTextField.text != "" && PasswordTextField.text != ""
+        {
+            Auth.auth().signIn(withEmail: EmailTextField.text!, password: PasswordTextField.text!) { (user, error) in
+                if user != nil
+                {
+                    print("Successfull")
+                }
+                else
+                {
+                  print("login failed")
+                }
+            }
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //view.setGradientBackground(colorOne: Colors.brightOrange, colorTwo: Colors.orange)
+        LoginButton.setGradientBackground(colorOne: Colors.brightOrange, colorTwo: Colors.orange)
     }
 
     override func didReceiveMemoryWarning() {
